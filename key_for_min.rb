7660592a351642177739f69b3c7b7862910b40a6
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-06 07:36:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if minimum = 0
       minimum = value
+      min_key = key
     end
     if value < minimum
       minimum = value
